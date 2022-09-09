976b554ea42065cdb7b8c8cd1085e67d0d210751
LandStart <320514639@qq.com>
2022-09-09 08:07:19 +0800

    修改请求的参数

--- base/src/main/java/com/dong/base/controller/infoController.java
+++ base/src/main/java/com/dong/base/controller/infoController.java
@@ -15,7 +15,7 @@ import org.springframework.web.client.RestTemplate;
 public class infoController {
 
 
-    @Autowired(required = false)
+    @Autowired(required =false )
     infoService infoService;
 
     @Autowired
